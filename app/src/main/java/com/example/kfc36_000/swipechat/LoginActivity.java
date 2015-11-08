d8b5ac0abf86c2a448a069b0726b88e29f71fbe5
Kaden Thomas <thomas.kaden4@hotmail.com>
2015-11-07 16:14:39 -0800

    FAB fix

--- app/src/main/java/com/example/kfc36_000/swipechat/LoginActivity.java
+++ app/src/main/java/com/example/kfc36_000/swipechat/LoginActivity.java
@@ -18,7 +18,7 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.sendingOptions);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
