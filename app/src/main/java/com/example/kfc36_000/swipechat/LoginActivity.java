09c5a861bf1f82ff68d273898c6914475677d125
ForisKuang <Forisk@uw.edu>
2015-11-07 17:44:08 -0800

    Troubleshooting sessio

--- app/src/main/java/com/example/kfc36_000/swipechat/LoginActivity.java
+++ app/src/main/java/com/example/kfc36_000/swipechat/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.kfc36_000.swipechat;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ImageButton butt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +23,17 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.sendingOptions);
-        fab.setOnClickListener(new View.OnClickListener() {
+        butt = (ImageButton)findViewById(R.id.openMessenger);
+
+        butt.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
+                Intent intent = new Intent(LoginActivity.this,MessageActivity.class);
+                startActivity(intent);
             }
         });
+
     }
 
     @Override
